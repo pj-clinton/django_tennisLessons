55e689ce87b05394a823c85c80e30b13d1d6fa77
PJ Clinton <github@intheory.bulc.club>
2023-01-03 20:25:15 -0600

    secretkey hidden, requirements file added

--- tennisLessons/settings.py
+++ tennisLessons/settings.py
@@ -8,8 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5bblk0%6)ip6=1w0mb0va^b2r5+ea$dsce2(osth*k+lf2i5^f'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
